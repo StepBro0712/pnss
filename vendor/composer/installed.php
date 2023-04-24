b09d6cbad7b28cd820bc23a78808631517983514
Stepan <stepa.super2021@mail.ru>
2023-04-24 18:24:01 +0700

    commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'jurapro/pop-it-mvc',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'stepbro0712/pnsk',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b49e2a68ea96553f0e52f8922d89fae89befc2c4',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'jurapro/pop-it-mvc' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'stepbro0712/pnsk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b49e2a68ea96553f0e52f8922d89fae89befc2c4',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
